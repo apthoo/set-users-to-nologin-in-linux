807fdb659ba0bb9a00c42d444fac4d09af7b4f11
Apthoo <202545694+apthoo@users.noreply.github.com>
2025-05-12 00:50:18 +0530

    main.sh

---
+++ main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Enter the usernames to set as /usr/sbin/nologin
+# -a to read into an array
+read -p "Enter the user names (space separated): " -a names  
+
+# Display the usernames
+echo "User names are:"
+echo "${names[@]}"
+
+# Loop through the usernames and check if they exist in /etc/passwd
+for user in "${names[@]}"; do
+    if grep -q "^$user:" /etc/passwd; then
+        echo "$user found and updating shell to nologin..."
+        sudo usermod -s /usr/sbin/nologin "$user"
+        echo "$user updated to 'nologin'"
+    else
+        echo "User $user not found."
+    fi
+done
